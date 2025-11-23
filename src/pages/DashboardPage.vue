d08f5c062617ddbd1fdf6e498f1c822073cdc7bc
ll23002 <ll23002@ues.edu.sv>
2025-11-23 14:59:23 -0600

    💄 Etiquetas principales en Dashboard  listas

--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -12,7 +12,7 @@
           <q-card-section class="row items-center">
             <div class="col">
               <div class="text-grey-5 text-caption text-uppercase">Ventas Hoy</div>
-              <div class="text-h4 text-weight-bolder q-mt-sm">$45,200</div>
+              <div class="text-h4 text-weight-bolder q-mt-sm">${{ ventas_hoy }}</div>
             </div>
             <div class="col-auto">
               <q-avatar
@@ -33,7 +33,7 @@
           <q-card-section class="row items-center">
             <div class="col">
               <div class="text-grey-5 text-caption text-uppercase">Por Cobrar</div>
-              <div class="text-h4 text-weight-bolder q-mt-sm">$12,150</div>
+              <div class="text-h4 text-weight-bolder q-mt-sm">${{ por_cobrar }}</div>
             </div>
             <div class="col-auto">
               <q-avatar
@@ -54,7 +54,7 @@
           <q-card-section class="row items-center">
             <div class="col">
               <div class="text-grey-5 text-caption text-uppercase">Pasivo Circulante</div>
-              <div class="text-h4 text-weight-bolder q-mt-sm">$8,500</div>
+              <div class="text-h4 text-weight-bolder q-mt-sm">${{pasivo_circulante}}</div>
             </div>
             <div class="col-auto">
               <q-avatar
@@ -107,7 +107,7 @@
           </q-card-section>
 
           <q-table
-            :rows="facturas"
+            :rows="rows"
             :columns="columns"
             row-key="id"
             flat
@@ -151,42 +151,116 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import VueApexCharts from 'vue3-apexcharts'
+import axios from 'axios'
 const apexchart = VueApexCharts
 
+const por_cobrar = ref(0)
+const ventas_hoy = ref(0)
+const pasivo_circulante = ref(0)
+
 const columns = [
-  { name: 'nombre', align: 'left', label: 'Factura Nombre', field: 'nombre', sortable: true },
-  { name: 'monto', align: 'left', label: 'Monto', field: 'monto', sortable: true },
-  { name: 'fecha', align: 'left', label: 'Fecha Emisión', field: 'fecha', sortable: true },
-  { name: 'estado', align: 'center', label: 'Estado', field: 'estado' },
-  { name: 'acciones', align: 'center', label: '', field: 'acciones' },
+  {
+    name: 'fecha',
+    label: 'FECHA',
+    align: 'left',
+    field: 'fecha',
+    sortable: true,
+  },
+  {
+    name: 'numero_factura',
+    label: 'NÚMERO DE FACTURA',
+    align: 'left',
+    field: 'numero_factura',
+    sortable: true,
+  },
+  {
+    name: 'subtotal',
+    label: 'SUBTOTAL',
+    align: 'left',
+    field: 'subtotal',
+    sortable: true,
+  },
+  {
+    name: 'impuesto',
+    label: 'IVA',
+    align: 'left',
+    field: 'impuesto',
+    sortable: true,
+  },
+  {
+    name: 'total',
+    label: 'TOTAL',
+    align: 'left',
+    field: 'total',
+    sortable: true,
+  },
+  {
+    name: 'estado',
+    label: 'ESTADO',
+    align: 'left',
+    field: 'estado',
+    sortable: true,
+  },
+  {
+    name: 'cliente',
+    label: 'CLIENTE',
+    align: 'left',
+    field: 'cliente_nombre',
+    sortable: true,
+  },
+  {
+    name: 'acciones',
+    label: 'ACCIONES',
+    align: 'center',
+  },
 ]
 
-//Debo cambiar esto
-const facturas = ref([
-  {
-    id: 1,
-    nombre: 'Facturación Digital 2023001',
-    monto: '$12,560.00',
-    fecha: '23/03/2023, 12:17:00',
-    estado: 'Completado',
-  },
-  {
-    id: 2,
-    nombre: 'Facturación Digital 2023002',
-    monto: '$9,900.00',
-    fecha: '22/03/2023, 13:17:00',
-    estado: 'Pendiente',
-  },
-  {
-    id: 3,
-    nombre: 'Facturación Digital 2023003',
-    monto: '$4,200.00',
-    fecha: '21/03/2023, 09:30:00',
-    estado: 'Completado',
-  },
-])
+const rows = ref([])
+async function obtenerFacturas() {
+  try {
+    const response = await axios.get('http://localhost:8000/api/contabilidad/facturas/obtener/')
+    rows.value = response.data
+  } catch (error) {
+    console.error('Error obteniendo facturas:', error)
+    rows.value = []
+  }
+}
+
+async function obtener_por_cobrar() {
+  try {
+    const response = await axios.get('http://localhost:8000/api/contabilidad/por_cobrar/')
+    por_cobrar.value = response.data.total_por_cobrar
+  } catch (error) {
+    console.error('Error obteniendo valor:', error)
+  }
+}
+
+async function obtener_ventas_hoy() {
+  try {
+    const response = await axios.get('http://localhost:8000/api/contabilidad/ventas_hoy/')
+    ventas_hoy.value = response.data.total_ventas
+  } catch (error) {
+    console.error('Error obteniendo valor:', error)
+  }
+}
+
+async function obtener_pasivo_circulante() {
+  try {
+    const response = await axios.get('http://localhost:8000/api/contabilidad/pasivo_circulante')
+    pasivo_circulante.value = response.data.total_saldo_actual
+  } catch (error) {
+    console.error('Error obteniendo valor:', error)
+  }
+}
+
+onMounted(() => {
+  obtenerFacturas()
+  obtener_por_cobrar()
+  obtener_ventas_hoy()
+  obtener_pasivo_circulante()
+})
 
 //Debo cambiar esto
 const series = ref([
@@ -216,50 +290,50 @@ const chartOptions = ref({
 
   stroke: {
     curve: 'smooth',
-    width: 4
+    width: 4,
   },
 
   xaxis: {
     categories: ['Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago'],
     labels: {
-      style: { colors: '#ccc' }
+      style: { colors: '#ccc' },
     },
     axisBorder: { show: false },
-    axisTicks: { show: false }
+    axisTicks: { show: false },
   },
 
   yaxis: {
     labels: {
-      style: { colors: '#ccc' }
+      style: { colors: '#ccc' },
     },
   },
 
   grid: {
     borderColor: '#535A6C',
     xaxis: {
-      lines: { show: true }
-    }
+      lines: { show: true },
+    },
   },
 
   theme: { mode: 'dark' },
 
   fill: {
-    type: "solid",
-    opacity: 0.3
+    type: 'solid',
+    opacity: 0.3,
   },
 
   markers: {
     size: 0,
     hover: {
-      size: 5
-    }
+      size: 5,
+    },
   },
 
   legend: {
     position: 'top',
     horizontalAlign: 'right',
-    offsetY: -20
-  }
+    offsetY: -20,
+  },
 })
 </script>
 
